e19bcd20e435b66b9e3e54291891903bfe08909a
AleksandraZheleva <aleksandra_zh@abv.bg>
2026-04-20 22:18:00 +0200

    Update artists.py

--- src/streaming/artists.py
+++ src/streaming/artists.py
@@ -8,7 +8,7 @@ Classes to implement:
 """
 from .tracks import Track
 class Artist:
-    def __init__(self, artist_id: str, name: str, genre: str) -> None:
+    def __init__(self, artist_id: str, name: str, genre: str):
         self.artist_id = artist_id
         self.name = name
         self.genre = genre
